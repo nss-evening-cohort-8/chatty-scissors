9b718db69c9b5eb1a0020345af90db3387d3b0d3
Raymond Arceneaux <raymond.techcoder@gmail.com>
2018-10-09 20:22:51 -0500

    added radio button

--- javascripts/components/users.js
+++ javascripts/components/users.js
@@ -1,1 +1,16 @@
-console.log('users.js')
+// console.log('users.js')
+
+
+let users = [];
+
+const radioButtonsBuilder = () => {
+    let domString = '';
+    for (let i =0;i<users.length;i++){
+        domString +=`<label class="btn btn-secondary active">
+        <input type="radio" name="options" id="${users[i]}">${users[i]}
+      </label>`;}
+      return domString
+}
+
+
+export{radioButtonsBuilder};
