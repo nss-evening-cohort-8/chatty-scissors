3b72d2a52ce635205f58b6572069a5bed7d5a6ac
Shane D Wilson <me@shanedwilson.com>
2018-10-09 19:59:58 -0500

    wip

--- javascripts/components/chatbox.js
+++ javascripts/components/chatbox.js
@@ -1,1 +1,18 @@
-console.log('chatbox.js');
+import {printToDom} from "../helpers/util";
+
+let messagesArray = [];
+let counter = 0;
+
+const messageBuilder = () => {
+    let newMessage = "";
+    for (let i = 0; i < messagesArray.length; i++) {
+    newMessage += `<p id="message${counter}">${messagesArray[i].userName}: `;
+    newMessage += `${messagesArray[i].message} `;
+    newMessage += `${messagesArray[i].timestamp}`
+    newMessage += `</p>`
+    }
+    counter++;
+    printToDom(newMessage, 'message-div')
+}
+
+export {messageBuilder};
