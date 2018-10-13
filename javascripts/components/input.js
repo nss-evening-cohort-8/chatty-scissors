2bc8f695c443bf98b2c14556ea25ed2623f48ab5
Mary West <westmarye1219@gmail.com>
2018-10-13 11:04:02 -0500

    working on conditional/ builder

--- javascripts/components/input.js
+++ javascripts/components/input.js
@@ -1,10 +1,31 @@
 import util from "../helpers/util.js";
+import chatbox from "./chatbox.js"
 
 const navBarReturnKey = () => {
     console.log('it works');
      util.printToDom(document.getElementById('text-box').value,'message-div')
      document.getElementById('text-box').value = ''
+//      if(returnkey === '13')
+//      input.navBarReturnKey()
+//      else {
+//         stringToPrint = ''
+// };
 
 };
 
+
 export default {navBarReturnKey};
+
+
+const  chatBoxMessageBuilder = () => {
+    for (let i = 0; i < users.length; i++) { 
+     newString += `<div class = "chat-container">`
+     newString += 
+}
+// things I need in this function
+// message builder function in chatbox
+// conditional statement to differentiate return keys
+//timestamp
+// radiobuttons checked
+
+
