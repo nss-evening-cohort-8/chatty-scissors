474a5c9e1027c9a6e205f51cc4d138270bfa4294
WaysideTester <dylanmurray2119@gmail.com>
2018-10-13 13:17:32 -0500

    message limit functioning

--- javascripts/components/input.js
+++ javascripts/components/input.js
@@ -1,10 +1,20 @@
 import util from "../helpers/util.js";
 
+const messageBox = document.getElementById("message-div");
+
 const navBarReturnKey = () => {
-    console.log('it works');
-     util.printToDom(document.getElementById('text-box').value,'message-div')
+    let newString = `<div>${document.getElementById('text-box').value}</div>`;
+    
+
+     util.printToDom(newString,'message-div')
      document.getElementById('text-box').value = ''
 
 };
 
-export default {navBarReturnKey};
+const maxMessageLimit = () => {
+    if (messageBox.children.length > 20){
+        messageBox.removeChild(messageBox.children[0]);
+    }
+}
+
+export default {navBarReturnKey, maxMessageLimit};
