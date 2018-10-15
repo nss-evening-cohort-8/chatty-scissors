269b8cde30332c83608df2f9f282c59cdc3a1081
WaysideTester <dylanmurray2119@gmail.com>
2018-10-15 00:19:22 -0500

    fixed user

--- javascripts/components/input.js
+++ javascripts/components/input.js
@@ -59,6 +59,9 @@ const profaneFilter = (inputValue) => {
             selectedUser = user[i].children[0].value;
             }
         }
+        if (selectedUser === '') {
+            alert('Please select a user name!')
+        } else {
         console.log("swear filter works");
         let newString = `<div id="message${counter}" class = "chatbox-container">`;
         newString += `<p class="message-user"> ${selectedUser}</p>`
@@ -67,6 +70,7 @@ const profaneFilter = (inputValue) => {
         newString += `</div>`
         counter++;
         util.printToDom(newString, 'message-div')
+    }
     }
     else {
         chatBoxMessageBuilder();
